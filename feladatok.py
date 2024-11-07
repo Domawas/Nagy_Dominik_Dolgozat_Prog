566267514f0bef768dfec0d9202e2dc1e1b3d793
Nagy Dominik (Szf_N_2024_2026) <nagy.dominik@diak.szamalk-szalezi.hu>
2024-11-07 09:41:55 +0100

    masodik

--- feladatok.py
+++ feladatok.py
@@ -1,4 +1,4 @@
-
+import random
 def elso_feladat():
     szam1 = int(input("Kérek egy páros számot: "))
     
@@ -7,4 +7,55 @@ def elso_feladat():
         szam1 = int(input("Kérek egy páros számot: "))  
     
     print(szam1)
+
+def masodik():
+    szamok=[]
+    i=0
+    igen=0
+    oszthato=0
+    while (i < 13):
+        szam=int(random.random()*140)+10
+        szamok.append(szam)
+        i+=1
+    
+    print(szamok)
+    while igen<13:
+        if szamok[igen]%3==0:
+            oszthato+=1
+        igen+=1
+    print(f"{oszthato}db 3-mal osztható")
+
+
+def harmadik(text,n):
+    text=text.upper()
+    if (len(text))<n:
+        print(f"Nincs {n}. karakter")
+    print(text[{n}]*3)
+
+
+
+def negyedik():
+    i = 0
+    nevek=[]
+    print("Kérem, adjon meg neveket! (A kilépéshez írjon '@' jelet)")
+    nev = input("Adj meg egy nevet (vagy @ a kilépéshez): ")
+    while nev != '@':
+        nev = input("Adj meg egy nevet (vagy @ a kilépéshez): ")
+        if nev=='@':
+            print(f"A felhasználó {i} nevet adott meg.")
+            return nevek
+
+        i += 1
+        nevek.append(nev)
+        
+
+
+
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import feladatok
 feladatok.elso_feladat()
+feladatok.masodik()
+i=""
+feladatok.harmadik(i,333)
+feladatok.negyedik()
